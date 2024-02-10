5771ef9e13f5909d18e66753daad7af68cc74a39
karnakar <thallapallikarnakar@gmail.com>
2024-02-10 13:25:33 -0800

    app

--- news-app/app.js
+++ news-app/app.js
@@ -1,6 +1,14 @@
 const express=require('express');
 const app=express();
+const mustacheExpress=require('mustache-express')
 const PORT=3000;
+//configuring view engine
+app.engine('mustache',mustacheExpress())
+app.set('views','./views');
+app.set('view engine','mustache')
+app.get('/register',(req,res)=>{
+    res.render('register')
+})
 app.listen(PORT,()=>{
 console.log(` server started ${PORT}`)
 })
