1f519dd1b6a8797ffef7ebfb36afb65d65ac4409
karnakar <thallapallikarnakar@gmail.com>
2024-02-10 15:05:21 -0800

    post

--- news-app/app.js
+++ news-app/app.js
@@ -1,14 +1,33 @@
-const express=require('express');
-const app=express();
-const mustacheExpress=require('mustache-express')
-const PORT=3000;
-//configuring view engine
+
+const express = require('express')
+const app = express()
+const mustacheExpress = require('mustache-express')
+const bodyParser = require('body-parser')
+const PORT = 3000
+
+// configuring your view engine
 app.engine('mustache',mustacheExpress())
-app.set('views','./views');
+app.set('views','./views')
 app.set('view engine','mustache')
-app.get('/register',(req,res)=>{
-    res.render('register')
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.post('/register',(req,res) => {
+
+  let username = req.body.username
+  let password = req.body.password
+
+  console.log(username)
+  console.log(password)
+
+  res.send("REGISTER")
+
 })
-app.listen(PORT,()=>{
-console.log(` server started ${PORT}`)
+
+app.get('/register',(req,res) => {
+  res.render('register')
+})
+
+app.listen(PORT,() => {
+  console.log(`Server has started on ${PORT}`)
 })
