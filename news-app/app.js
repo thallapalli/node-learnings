ace0b2395a76080d2c6cc01d4635c1637033efef
karnakar <thallapallikarnakar@gmail.com>
2024-02-10 17:50:40 -0800

    redirect added

--- news-app/app.js
+++ news-app/app.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser')
 const PORT = 3000
 const pgp = require('pg-promise')()
 const bcrypt = require('bcrypt')
+const session = require('express-session');
+
 const username = "postgres"
 const password = "Tinku_2015"
 const host = "localhost"
@@ -20,28 +22,46 @@ const db = pgp(connectionString);
 app.engine('mustache', mustacheExpress())
 app.set('views', './views')
 app.set('view engine', 'mustache')
+app.get('/users/articles',(req,res)=>{
+res.render('articles',{username:req.session.user.username})
+})
+app.use(session({
+  secret:'lhadhlsdalh',
+  resave: false,
+  saveUninitialized: false
+}))
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.get('/login', (req, res) => {
-  res.render('login')
+  res.render('login',req.session.username);
 })
 app.post('/login', (req, res) => {
   let username = req.body.username
   let password = req.body.password
  
   db.oneOrNone('SELECT userid,username,password FROM newsdb.users WHERE username=$1', [username])
-    .then((user) => {
+    .then(user => {
     
-      if (user) {
-        console.log("comparing")
-        bcrypt.compare(password, user.password, function(error, result) {
-
-          if (result) {
-            res.send("SUCCESS")
+      if(user){
+       
+       let haspwd=user.password;
+       let userid=user.userid;
+        bcrypt.compare(password, haspwd, function(error, result) {
+        
+         if(result) {
+         
+            if(req.session){
+           
+              req.session.user= {userId: user.userid,username: user.username}
+              console.log(req.session.user);
+              res.redirect('/users/articles')
+             
+            }
+            
           } else {
             res.render('login', { message: "Invalid username or password" })
           }
-        })
+      })
       } else {
         res.render('login', { message: "Invalid username or password" })
       }
